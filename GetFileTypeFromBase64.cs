0a4923f2dbadaf065685e8267dd6e1c3963141cd
Bambam Deo <11490008+bambamdeo@users.noreply.github.com>
2020-01-20 12:39:19 +0530

    Update GetFileTypeFromBase64.cs

--- GetFileTypeFromBase64.cs
+++ GetFileTypeFromBase64.cs
@@ -1,29 +1,29 @@
-public static string GetFileExtension(string base64String)
-{
-var data = base64String.Substring(0, 5);
+private string GetFileExtension(string base64String)
+        {
+            var data = base64String.Substring(0, 5);
 
-switch (data.ToUpper())
- {
-     case "IVBOR":
-        return "png";
-     case "/9J/4":
-         return "jpg";
-     case "AAAAF":
-         return "mp4";
-     case "JVBER":
-         return "pdf";
-     case "AAABA":
-         return "ico";
-     case "UMFYI":
-         return "rar";
-     case "E1XYD":
-         return "rtf";
-     case "U1PKC":
-        return "txt";
-     case "MQOWM":
-     case "77U/M":
-        return "srt";
-     default:
-        return string.Empty;
- }
-}
+            switch (data.ToUpper())
+            {
+                case "IVBOR":
+                    return "png";
+                case "/9J/4":
+                    return "jpg";
+                case "AAAAF":
+                    return "mp4";
+                case "JVBER":
+                    return "pdf";
+                case "AAABA":
+                    return "ico";
+                case "UMFYI":
+                    return "rar";
+                case "E1XYD":
+                    return "rtf";
+                case "U1PKC":
+                    return "txt";
+                case "MQOWM":
+                case "77U/M":
+                    return "srt";
+                default:
+                    return string.Empty;
+            }
+        }
