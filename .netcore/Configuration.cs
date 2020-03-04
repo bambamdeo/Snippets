9e06d49cd5aadb32cc99f5dc191bfa9c2ee200e5
Bambam Deo <11490008+bambamdeo@users.noreply.github.com>
2020-03-04 17:32:18 +0530

    Update Configuration.cs

--- .netcore/Configuration.cs
+++ .netcore/Configuration.cs
@@ -21,4 +21,21 @@ public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerF
 
  
   
+  ////Use vs Run
+   app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("<html><body>");
+        await context.Response.WriteAsync("<div>Inside middleware defined using app.Use</div>");
+        await next();
+        await context.Response.WriteAsync("</body></html>");
+    });
+  //contnue to next middleware
+ 
+    app.Run(async context => { 
+       await context.Response.WriteAsync("<div>Inside middleware defined using app.Run</div>"); 
+    });
+  //stop and return
+  
+  
+  
 }
